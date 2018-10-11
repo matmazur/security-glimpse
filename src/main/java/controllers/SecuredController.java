c963bdc7e99c81aaaf8b3711353f366ec82a35fe
Bingo Bobo <matmazur90@gmail.com>
2018-10-11 14:18:30 +0200

    changed a bit property files, created basic authorization under url /secure in web.xml

--- src/main/java/controllers/SecuredController.java
+++ src/main/java/controllers/SecuredController.java
@@ -1,11 +1,23 @@
 package controllers;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Principal;
 
 @WebServlet("/secure")
 public class SecuredController extends HttpServlet {
 
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        Principal principal = req.getUserPrincipal();
+        String name = principal.getName();
+
+        resp.getWriter().println("secured area, hello user " + name);
+    }
 }
