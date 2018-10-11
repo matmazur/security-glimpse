4eab5f43fe1529f375988cce9e01eb6475786251
Bingo Bobo <matmazur90@gmail.com>
2018-10-11 13:50:02 +0200

    added few empty packages, in one there is one servlet for future development

---
+++ src/main/java/controllers/SecuredController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/secure")
+public class SecuredController extends HttpServlet {
+
+
+
+}
