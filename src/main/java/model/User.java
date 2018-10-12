64bdf4ef1afe2590fa741660f55d6ee1b2a1de1b
Bingo Bobo <matmazur90@gmail.com>
2018-10-12 14:22:49 +0200

    added Entity  class User

---
+++ src/main/java/model/User.java
@@ -0,0 +1,68 @@
+package model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @NotNull
+    @ElementCollection
+    @CollectionTable(name = "roles",
+            joinColumns = @JoinColumn(name = "username", referencedColumnName = "username"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.roles.add(role);
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
