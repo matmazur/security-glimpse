f5122ca0695b582e7be8cb3bac9777edd06a3b93
Bingo Bobo <matmazur90@gmail.com>
2018-10-12 14:36:15 +0200

    created quick User repository class

---
+++ src/main/java/beans/UserRepository.java
@@ -0,0 +1,19 @@
+package beans;
+
+import model.User;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class UserRepository {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void add(User user) {
+        entityManager.persist(user);
+    }
+
+}
