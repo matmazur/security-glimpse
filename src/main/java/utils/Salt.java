033ccb55a08cd86d187c7831ae2c0be229501d6c
Bingo Bobo <matmazur90@gmail.com>
2018-10-13 12:12:06 +0200

    created Salt as a Singleton

---
+++ src/main/java/utils/Salt.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class Salt {
+
+    private static final String PRE = "SDtge4534%$#TrSA";
+    private static final String POST = "&^%$&hgfHGFD3242W";
+
+    public static Salt salt = new Salt();
+
+    private Salt() {
+    }
+
+
+    public static String salter(String string) {
+
+        return PRE + string + POST;
+    }
+}
